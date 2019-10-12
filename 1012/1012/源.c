db5e666e3c4d98812c6740e54119f0ee28c2ce54
GeniusPanYiHao <1542029272@qq.com>
2019-10-12 23:56:26 +0800

    1012

---
+++ 1012/1012/源.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define cow	3
+#define row	3
+int input(int arr[cow][row], int key)
+{
+	int i = 0;
+	int j = row - 1;
+	while ((j >= 0) && (i <= 2))
+	{
+		if (arr[i][j] == key)
+		{
+			return 1;
+		}
+		else if (arr[i][j] < key)
+		{
+			++i;
+		}
+		else
+		{
+			--j;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int arr[cow][row] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int key = 0;
+	int ret = 0;
+	printf("输入要查找的数：");
+	scanf("%d", &key);
+	ret = input(arr, key);
+	if (ret == 1)
+	{
+		printf("杨氏矩阵中有:%d\n", key);
+	}
+	else
+	{
+		printf("杨氏矩阵中没有:%d\n", key);
+	}
+	return 0;
+}
