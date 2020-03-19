e8e542dba05480c7145c29850415a8eeafee1f55
GeniusPanYiHao <1542029272@qq.com>
2020-03-19 21:41:54 +0800

    20200319

---
+++ 20200319/20200319/main.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	/*double a = 1, b = 2, c = 3;
+	printf("%f  %f  %f\n", a, b, c);*/
+	/*long long a = 1, b = 2, c = 3;
+	printf("%lld  %lld  %lld\n", a, b, c);*/
+	long a = 1, b = 2, c = 3;
+	printf("%ld  %ld  %ld\n", a, b, c);
+	return 0;
+}
