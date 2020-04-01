a5654a6f95ab4eb6b4c41f7a9629caa4ac4de9fc
GeniusPanYiHao <1542029272@qq.com>
2020-04-01 19:27:29 +0800

    题

---
+++ 刷题吧/刷题吧/main.cpp
@@ -0,0 +1,71 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//void Traingle(int T,double a,double b,double c)
+//{
+//	double p;
+//	double s;
+//	if ((a + b) > c && (a + c) > b && (b + c)>a)
+//	{
+//		p = (a + b + c)*1.000 / 2;
+//		s = sqrt(p*(p - a)*(p - b)*(p - c));
+//		printf("%.3f\n", s);
+//	}
+//	else
+//		printf("%d\n", -1);
+//}
+//int main()
+//{
+//	int T;
+//	cin >> T;
+//	double a, b,c;
+//	for (int i = 0; i < T; i++)
+//	{
+//		cin >> a >> b >> c;
+//		Traingle(T, a, b, c);
+//		if (i!=T-1)
+//		{
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+int fun(int a)
+{
+	int i;
+	for (i = 2; i < a; i++)
+	{
+		if (a%i == 0)
+			return 0;
+	}
+	return 1;
+}
+int fun1(int n)
+{
+	int m = 1;
+	int count = 0;
+	while (n--)
+	{
+		m++;
+		if (fun(m)&&n%m==0)
+		{
+			count++;
+		}
+		if (count == 2)
+			return m;
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	cout << fun1(n) << endl;
+}
+
--- 爬楼梯问题/爬楼梯问题/main.cpp
+++ 爬楼梯问题/爬楼梯问题/main.cpp
@@ -1,26 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//using namespace std;
+//long long input(long long n)
+//{
+//	if (n < 2)
+//		return n;
+//	long long pre1 = 1;
+//	long long pre2 = 1;
+//	long long pre3 = 2;
+//	long long cur = 0;
+//	for (int i = 4; i <= n; i++)
+//	{
+//		cur = pre1 + pre3;
+//		pre1 = pre2;
+//		pre2 = pre3;
+//		pre3 = cur;
+//	}
+//	return pre3;
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	cout<<input(n)<<endl;
+//}
 #include<iostream>
 using namespace std;
-long long input(long long n)
-{
-	if (n < 2)
-		return n;
-	long long pre1 = 1;
-	long long pre2 = 1;
-	long long pre3 = 2;
-	long long cur = 0;
-	for (int i = 4; i <= n; i++)
-	{
-		cur = pre1 + pre3;
-		pre1 = pre2;
-		pre2 = pre3;
-		pre3 = cur;
-	}
-	return pre3;
-}
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	cout<<input(n)<<endl;
+	cout << 2021 << endl;
 }
